8ab5fa3aaead1ba308c4f97811dab9c85b2fec50
Sudheeshshetty <sudheeshshetty@gmail.com>
2020-06-01 17:43:25 +0530

    Proper management of like and dislikes

--- common/models/dislike.js
+++ common/models/dislike.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(Dislike) {
-
+module.exports = function (Dislike) {
+    Dislike.observe('after save', function (ctx, next) {
+        var Video = Dislike.app.models.video;
+        var disliked = ctx.instance.selected;
+        Video.findById(ctx.instance.videoId, (err, foundVideo) => {
+            var totaldislikes = foundVideo.totaldislikes
+            if (disliked) {
+                totaldislikes += 1;
+            } else {
+                totaldislikes -= 1;
+            }
+            foundVideo.updateAttributes({ totaldislikes: totaldislikes });
+            next();
+        });
+    });
 };
--- common/models/like.js
+++ common/models/like.js
@@ -1,5 +1,18 @@
 'use strict';
 
-module.exports = function(Like) {
-
+module.exports = function (Like) {
+    Like.observe('after save', function (ctx, next) {
+        var Video = Like.app.models.video;
+        var liked = ctx.instance.selected;
+        Video.findById(ctx.instance.videoId, (err, foundVideo) => {
+            var totallikes = foundVideo.totallikes
+            if (liked) {
+                totallikes += 1;
+            } else {
+                totallikes -= 1;
+            }
+            foundVideo.updateAttributes({ totallikes: totallikes });
+            next();
+        });
+    });
 };
